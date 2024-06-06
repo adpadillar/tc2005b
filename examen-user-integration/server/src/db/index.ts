aad50240434df3c64f3b31d6293e7d3f93b8204d
Axel Padilla <adpadillar25@gmail.com>
2024-06-05 19:33:41 -0600

    refactored db into singleton

--- examen-user-integration/server/src/db/index.ts
+++ examen-user-integration/server/src/db/index.ts
@@ -1,9 +1,29 @@
 import { Pool } from "pg";
 
-const db = new Pool({
-  // Use the Pool class to create a new pool
-  connectionString:
-    "postgres://postgres.oigisgmjkmrhmylxutpu:WDXN3XeR5qFIYBL6@aws-0-us-west-1.pooler.supabase.com:5432/postgres",
-});
+class Database {
+  private static instance: Database;
+  private pool: Pool;
+
+  private constructor() {
+    this.pool = new Pool({
+      // Use the Pool class to create a new pool
+      connectionString:
+        "postgres://postgres.oigisgmjkmrhmylxutpu:WDXN3XeR5qFIYBL6@aws-0-us-west-1.pooler.supabase.com:5432/postgres",
+    });
+  }
+
+  public static getInstance(): Database {
+    if (!Database.instance) {
+      Database.instance = new Database();
+    }
+    return Database.instance;
+  }
+
+  public getPool(): Pool {
+    return this.pool;
+  }
+}
+
+const db = Database.getInstance().getPool();
 
 export default db;
